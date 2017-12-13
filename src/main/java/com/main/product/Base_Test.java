953bddb103e4d29dc1b38eeeba438928aaf383ee
Unknown <ashwani@fourtek.com>
2017-12-13 11:44:43 +0530

    Adding Testng jar and Made some other changes

---
+++ src/main/java/com/main/product/Base_Test.java
@@ -0,0 +1,38 @@
+package com.main.product;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+public class Base_Test {
+	
+	public static WebDriver driver;
+    public static Properties prop;
+    public static File file;
+    
+    //Read the property file
+    public static void read_property_file()
+    {
+    try {
+        file = new File(System.getProperty("user.dir") + "src/resource/java/com/resources/Config.prop");
+        FileInputStream file_read = new FileInputStream(file);
+        prop = new Properties();
+        prop.load(file_read);
+    
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } 
+    catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+    
+    
+    
+}
+
+
